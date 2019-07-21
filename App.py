1a4c0cb4a9074763ab6b3b63fb86ccbe145d2923
Jaehee <jaeheeyoum@gmail.com>
2019-07-21 16:30:48 +0900

    Template HTML을 추가한 버젼

--- App.py
+++ App.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
    return 'this is my homepage!'
+
+@app.route('/jaehee')
+def jaehee():
+    return render_template('introduction.html')
 
 if __name__ == '__main__':
    app.run()
