0b4a6bac0a9e394a423e81fbb38875b0765db89f
Jaehee <jaeheeyoum@gmail.com>
2019-08-23 21:43:37 +0900

    Make filename lower case

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,10 +7,27 @@ app = Flask(__name__)
 def index():
    return 'this is my homepage!'
 
-
 @app.route('/profile')
 def profile():
-    return render_template('introduction.html')
+    id = request.args.get ('id')
+    name = request. args.get ('name')
+    return "info : " + id + ":" + name
+
+@app.route('/register', methods=['POST'])
+def register():
+    header = request.headers['Content-Length']
+    body = request.get_data(as_text=True)
+    return body
+
+@app.route('/writeComplete', methods=['POST'])
+def writeComplete():
+    contents = request.get_data(as_text=True)
+    return render_template('writeComplete.html', contentsInHtml=contents)
+
+@app.route('/write')
+def write():
+    return render_template('write.html')
+
 
 if __name__ == '__main__':
    app.run(debug=True)
